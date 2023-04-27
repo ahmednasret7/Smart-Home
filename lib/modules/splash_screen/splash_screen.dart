927527073b6e0a2a364834a9d1582a1a5f743b27
AhmedSasa07 <ahmed.mos01129995098@gmail.com>
2023-04-28 02:44:38 +0300

    added splash_screen.dart when opening the app

---
+++ lib/modules/splash_screen/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:hi_ddd_test1/modules/smart_home_home_screen/smart_home_home_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+class splash_screen extends StatelessWidget {
+
+  @override
+  Color background_color = const Color(0xffFaFafa);
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splashIconSize: 500,
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: background_color,
+        pageTransitionType: PageTransitionType.fade,
+        splash: const CircleAvatar(
+          backgroundImage: AssetImage('assets/images/Handdrawn Outdoor Travel Logo Design.png'),
+          radius: 400,
+        ),
+        nextScreen: HomeScreen());
+  }
+}
